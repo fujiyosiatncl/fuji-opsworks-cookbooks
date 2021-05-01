b65476461981b7b8f7b1b031aa3dff56fc79b84c
fujiyosi <fujiyosi@ncl.sakura.ne.jp>
2021-05-02 00:00:56 +0900

    update

--- ex9-3/recipes/default.rb
+++ ex9-3/recipes/default.rb
@@ -1,77 +1,83 @@
 #
-# [
-#   data_bag_item[
-#     "aws_opsworks_app",
-#     "nodejs_sample_app",
+# search('aws_opsworks_app') ->
+#   [
 #     {
-#       "app_id"=>"ced43a83-cea9-4274-90d0-2719b161776e",
-#       "app_source"=>{
-#         "password"=>nil,
-#         "revision"=>nil,
-#         "ssh_key"=>nil,
-#         "type"=>"git",
-#         "url"=>"https://github.com/awslabs/opsworks-windows-demo-nodejs.git",
-#         "user"=>nil
+#       "app_id": "ced43a83-cea9-4274-90d0-2719b161776e",
+#       "app_source": {
+#         "password": null,
+#         "revision": null,
+#         "ssh_key": null,
+#         "type": "git",
+#         "url": "https://github.com/awslabs/opsworks-windows-demo-nodejs.git",
+#         "user": null
 #       },
-#       "attributes"=>{
-#         "auto_bundle_on_deploy"=>true,
-#         "aws_flow_ruby_settings"=>{},
-#         "document_root"=>nil,
-#         "rails_env"=>nil
+#       "attributes": {
+#         "auto_bundle_on_deploy": true,
+#         "aws_flow_ruby_settings": {
+#         },
+#         "document_root": null,
+#         "rails_env": null
 #       },
-#       "data_sources"=>[
+#       "data_sources": [
 #         {
-#           "arn"=>"arn:aws:rds:ap-northeast-1:142901573738:db:fuji-rdsmaria1",
-#           "type"=>"RdsDbInstance",
-#           "database_name"=>"food"
+#           "arn": "arn:aws:rds:ap-northeast-1:142901573738:db:fuji-rdsmaria1",
+#           "type": "RdsDbInstance",
+#           "database_name": "food"
 #         }
 #       ],
-#       "domains"=>[
+#       "domains": [
 #         "nodejs_sample_app"
 #       ],
-#       "enable_ssl"=>false,
-#       "environment"=>{
-#         "APP_ADMIN_EMAIL"=>"admin@example.com"
+#       "enable_ssl": false,
+#       "environment": {
+#         "APP_ADMIN_EMAIL": "admin@example.com"
 #       },
-#       "name"=>"Node.js Sample App",
-#       "shortname"=>"nodejs_sample_app",
-#       "ssl_configuration"=>{
-#         "certificate"=>nil,
-#         "private_key"=>nil,
-#         "chain"=>nil
+#       "name": "Node.js Sample App",
+#       "shortname": "nodejs_sample_app",
+#       "ssl_configuration": {
+#         "certificate": null,
+#         "private_key": null,
+#         "chain": null
 #       },
-#       "type"=>"other",
-#       "deploy"=>true,
-#       "id"=>"nodejs_sample_app"
+#       "type": "other",
+#       "deploy": true
 #     }
 #   ]
-# ]
 #
 #
-# aws_opsworks_rds_db_instance
+# https://docs.aws.amazon.com/opsworks/latest/userguide/data-bag-json-rds.html
+#
+# search('aws_opsworks_rds_db_instance') ->
+#   [
+#     {
+#       "rds_db_instance_arn": "arn:aws:rds:ap-northeast-1:142901573738:db:fuji-rdsmaria1",
+#       "db_instance_identifier": "fuji-rdsmaria1",
+#       "db_user": "admin",
+#       "db_password": "8i3Yqgbgxq9RD9dspNf4",
+#       "region": "ap-northeast-1",
+#       "address": "fuji-rdsmaria1.c7bguirq0zee.ap-northeast-1.rds.amazonaws.com",
+#       "port": 3306,
+#       "engine": "mariadb",
+#       "missing_on_rds": false
+#     }
+#   ]
 #
 
-result = search('aws_opsworks_rds_db_instance')
-raise result.inspect
+app = search('aws_opsworks_app').first
+dbname = app['data_sources']['database_name']
+dbins = search('aws_opsworks_rds_db_instance').first
+pass = dbins['db_password']
+user = dbins['db_user']
+host = dbins['address']
 
-database = app[:database]
-host = database[:host]
-user = database[:username]
-pass = database[:password]
-dbname = database[:database]
+package "mariadb" do
+  action :install
+end
 
-sql = 
-  "insert into food values (101, '#{host}', 'NA', 99); " +
-  "insert into food values (102, '#{dbname}', 'NA', 99); " +
-  "insert into food values (103, '#{user}', 'NA', 99); " +
-  "insert into food values (104, '#{pass}', 'NA', 99); "
-
-commands = <<-"EOS"
-echo "Fuji start"
-mysql -h #{host} -u #{user} -p#{pass} -D #{dbname} -e "#{sql}"
-echo "Fuji end"
-EOS
-
-bash "DB connectivity test" do
-  code commands
+script "DB connectivity test" do
+  interpreter "bash"
+  user "root"
+  sql = "insert into tempura values ('#{host} / #{user} / #{pass} / #{dbname}')"
+  code "echo \"#{sql}\" > /tmp/hoge.txt"
+  code "mysql -h #{host} -u #{user} -p#{pass} -D #{dbname} -e \"#{sql}\""
 end
